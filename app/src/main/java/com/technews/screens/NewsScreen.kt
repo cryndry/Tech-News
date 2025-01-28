36ae45b7a5319ef0cf24eb32e482eec7fd1eb7a7
cryndry <ismailkaraagac94@gmail.com>
2025-01-28 15:15:39 +0300

    add "Back to Top" button to News Screen

--- app/src/main/java/com/technews/screens/NewsScreen.kt
+++ app/src/main/java/com/technews/screens/NewsScreen.kt
@@ -14,16 +14,25 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -46,22 +55,64 @@ fun NewsScreen(newsViewModel: NewsViewModel) {
     val newsFetchState = newsViewModel.fetchStatus.value
     val navController = LocalNavController.current
 
-    Scaffold { innerPadding ->
-        Box(modifier = Modifier
-            .padding(innerPadding)
-            .fillMaxSize()) {
+    val coroutineScope = rememberCoroutineScope()
+    val lazyListState = rememberLazyListState()
+    val isScrolled = remember {
+        derivedStateOf {
+            lazyListState.firstVisibleItemIndex > 1
+        }
+    }
+
+    Scaffold(
+        floatingActionButton = {
+            if (isScrolled.value) {
+                Surface(
+                    modifier = Modifier.height(72.dp).width(72.dp)
+                        .clickable {
+                            coroutineScope.launch {
+                                lazyListState.animateScrollToItem(0)
+                            }
+                        },
+                    shape = RoundedCornerShape(50),
+                    color = Color.Red,
+                ) {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        Icon(
+                            Icons.Filled.KeyboardArrowUp,
+                            contentDescription = null,
+                            modifier = Modifier.height(48.dp).width(48.dp),
+                            tint = Color.White,
+                        )
+                    }
+                }
+            }
+        }
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        ) {
             when {
                 newsFetchState.loading && newsFetchState.news.isEmpty() -> {
                     CircularProgressIndicator(
                         modifier = Modifier.align(Alignment.Center)
                     )
                 }
+
                 newsFetchState.error != null -> {
                     Text(newsFetchState.error!!)
                 }
+
                 else -> {
                     LazyColumn(
-                        modifier = Modifier.fillMaxSize().padding(16.dp)
+                        state = lazyListState,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(16.dp),
                     ) {
                         itemsIndexed(newsFetchState.news) { index, newsItem ->
                             NewsScreenItem(newsItem) {
@@ -69,7 +120,10 @@ fun NewsScreen(newsViewModel: NewsViewModel) {
                                     withContext(Dispatchers.IO) {
                                         newsViewModel.getNewsDetail(newsItem.url, newsItem.source)
                                         withContext(Dispatchers.Main) {
-                                            navController.currentBackStackEntry?.savedStateHandle?.set("news", newsItem)
+                                            navController.currentBackStackEntry?.savedStateHandle?.set(
+                                                "news",
+                                                newsItem
+                                            )
                                             navController.navigate(ScreenManager.NewsDetailScreen.route)
                                         }
                                     }
@@ -103,7 +157,9 @@ fun NewsScreenItem(newsItem: News, onClick: () -> Unit) {
             Image(
                 painter = rememberAsyncImagePainter(newsItem.image),
                 contentDescription = null,
-                modifier = Modifier.height(200.dp).fillMaxWidth(),
+                modifier = Modifier
+                    .height(200.dp)
+                    .fillMaxWidth(),
                 contentScale = ContentScale.FillWidth,
             )
             Spacer(Modifier.height(8.dp))
@@ -116,7 +172,7 @@ fun NewsScreenItem(newsItem: News, onClick: () -> Unit) {
                 ),
             )
             Spacer(Modifier.height(16.dp))
-            Row (
+            Row(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
@@ -141,8 +197,10 @@ fun NewsScreenItem(newsItem: News, onClick: () -> Unit) {
 
 @Composable
 fun BottomLoadingIndicator(callback: () -> Unit) {
-    Row (
-        modifier = Modifier.height(40.dp).fillMaxWidth(),
+    Row(
+        modifier = Modifier
+            .height(40.dp)
+            .fillMaxWidth(),
         horizontalArrangement = Arrangement.Center,
     ) {
         CircularProgressIndicator()
